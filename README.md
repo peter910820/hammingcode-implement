ca4a807824189604b8b980686d7fa2c054f52db4
peter910820 <peter910820@gmail.com>
2024-07-12 02:33:15 +0800

    all method is complete

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # hammingcode-implement
 hamming code implement with python
+
+use **-E** to encode, **-D** to decode.
+
+![picture01](/p01.png)
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from argparse import ArgumentParser
 from functools import reduce
 
+def parse_args() -> object:
+    parse = ArgumentParser(description='hamming code implement for python')
+    parse.add_argument('-E', '--encode', type=str)
+    parse.add_argument('-D', '--decode', type=str)
+    args = parse.parse_args()
+    return args
+
 class hammingcode_implement:
-    def hammingcode_encode(self, data: str) -> list:
+    def hammingcode_encode(self, data: str) -> str:
         hammingcode_len = len(data)
         k = 1
         while 2**k-k < hammingcode_len+1:
@@ -14,8 +22,8 @@ class hammingcode_implement:
             hammingcode_list.insert(retention_bit-1, None)
             retention_bit *= 2
             check_bit_len += 1
-        caculate = [int(index+1) for index, values in enumerate(hammingcode_list) if values == "1"]
-        check_code = bin(reduce(lambda x, y: x ^ y, caculate)).lstrip("0b") #xor
+        caculate = [int(index+1) for index, values in enumerate(hammingcode_list) if values == '1']
+        check_code = bin(reduce(lambda x, y: x ^ y, caculate)).replace('0b', '') #xor
         if len(check_code) < check_bit_len: #zero padding
             check_code = check_code.zfill(check_bit_len)
         check_code = list(check_code)
@@ -24,9 +32,27 @@ class hammingcode_implement:
                 hammingcode_list[index] = check_code.pop()
         return ''.join(hammingcode_list)
         
-    def hammingcode_decode(self):
-        pass
-
-run = hammingcode_implement()
-
-print(run.hammingcode_encode("1"))
+    def hammingcode_decode(self, data: str) -> str:
+        data = list(data)
+        check_bits = [index+1 for index, cheker in enumerate(data) if cheker == '1']
+        check_bit = int(bin(reduce(lambda x, y: x^y, check_bits)).replace('0b', ''), 2)
+        if check_bit != 0:
+            change_bit = int(data[check_bit-1]) | int(data[check_bit-1])
+            change_bit = 1 if change_bit == 0 else 0
+            data[check_bit-1] = str(change_bit)
+            return f'an errors in the data, error bits is {check_bit}, correct data is {"".join(data)}'
+        elif check_bit == 0:
+            return 'there are no errors in the data'
+        else:
+            return 'another state'
+        
+if __name__ == '__main__':
+    args = parse_args()
+    run = hammingcode_implement()
+    try:
+        if args.encode:
+            print(run.hammingcode_encode(args.encode))
+        if args.decode:
+            print(run.hammingcode_decode(args.decode))
+    except:
+        print('error!')
